2735759365419d812e61fde775a4aee754ac347f
ezequias <ezequiassam@hotmail.com>
2021-04-21 12:37:12 -0300

    Add project

---
+++ src/main/java/com/capgemini/bank/capemibank/CapemibankApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.bank.capemibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapemibankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapemibankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capgemini/capemibank/CapemibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.capemibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapemibankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
