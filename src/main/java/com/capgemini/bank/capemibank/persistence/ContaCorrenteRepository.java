eebd34b37dffde2197d792c47a3636732a16f8fd
ezequias <ezequiassam@hotmail.com>
2021-04-21 15:04:10 -0300

    Add persistence

---
+++ src/main/java/com/capgemini/bank/capemibank/persistence/ContaCorrenteRepository.java
@@ -0,0 +1,7 @@
+package com.capgemini.bank.capemibank.persistence;
+
+import com.capgemini.bank.capemibank.model.ContaCorrente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaCorrenteRepository extends CrudRepository<ContaCorrente, Long> {
+}
